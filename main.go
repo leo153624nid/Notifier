cd366f1c16deed989535d5ad1d156324ee95c386
Aleksey Chaykin <barjastroy@gmail.com>
2026-09-01 17:51:21 +0300

    refactor

--- main.go
+++ main.go
@@ -97,22 +97,18 @@ func notificationHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// message, status := n.Send()
-	// fmt.Fprintln(w, message)
-	// fmt.Fprintln(w, "status:", status)
 	w.Header().Set("Content-type", "application/json")
 	w.Write(js)
 }
 
 func main() {
+	fmt.Printf("Starting %s %s on: 8080\n", appName, appVersion)
+
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/api/notifications", notificationHandler)
-
-	fmt.Println("Starting server on: 8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
-
 }
