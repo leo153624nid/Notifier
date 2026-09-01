ac7f401013894106b5a65987fa408cc03ad43513
Aleksey Chaykin <barjastroy@gmail.com>
2026-09-01 12:02:42 +0300

    update

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
-// package golearning
 package main
 
 import "fmt"
 
+const appVersion = "1.0.0"
+
 func main() {
-	fmt.Println("Notifier service is running...")
+	recipient := "user@example.com"
+	subject := "Deploy done"
+	retryCount := 3
+	timeout := 3.5 // float64
+	isUrgent := false
+
+	fmt.Println(recipient, subject, retryCount, timeout, isUrgent)
+	fmt.Println(appVersion)
 }
